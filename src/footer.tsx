d7284ee942f9f8f45b87b4ad0241c8f2c43b9661
Himanshu <himanshu.kumar.23001@gmail.com>
2026-05-12 21:17:48 +0530

    initial commit

---
+++ src/footer.tsx
@@ -0,0 +1,38 @@
+const footer = () => {
+  return (
+    <footer>
+      <ul className="list-1">
+        <li>
+          <a href="#">Dashboard</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+      </ul>
+      <ul className="list-2">
+        <li>
+          <a href="#">Blog Posts</a>
+        </li>
+        <li>
+          <a href="#">Discord</a>
+        </li>
+      </ul>
+      <ul className="list-3">
+        <li>
+          <a href="#">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="#">Terms & Conditions</a>
+        </li>
+      </ul>
+      <p className="copyright">© Reusable Footer.All Rights Reserved.</p>
+      <div className="icons">
+        <a href="#">🎮</a>
+        <a href="#">💻</a>
+        <a href="#">🏀</a>
+      </div>
+    </footer>
+  );
+};
+
+export default footer;
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import Footer from "./footer";
+import "./styles.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Footer />
+  </StrictMode>,
+);
